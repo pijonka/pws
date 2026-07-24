c7be1995bad78d64293b99ea4a9201e3c1d38af1
pijonka <jon.d.elkouby@gmail.com>
2026-07-24 17:55:34 -0400

    chore(q3): save unfinished changes of nested if system

--- data/q3/mcgill-billboard-dataset-extractor.py
+++ data/q3/mcgill-billboard-dataset-extractor.py
@@ -20,7 +20,7 @@ write_dataset = []
 
 # define hardcoded I - V - IV - iv chord progression object, with strings of the progression in each key
 FORMS_OF_CHORD_PROG = {
-    "c_maj": "C:maj|G:maj|F:maj|F:min",
+    "c_maj": ["C:maj", "G:maj", "F:maj", "F:min"],
     "g_maj": "G:maj|D:maj|C:maj|C:min",
     "d_maj": "D:maj|A:maj|G:maj|G:min",
     "a_maj": "A:maj|E:maj|D:maj|D:min",
@@ -45,49 +45,34 @@ LIST_REMOVE_OUT_STRING = [
     " ",
 ]
 
-# formats salami_chords.txt in a proper format;
-# TODO: currently does not correctly format "X:7" chords
-def parse_salami_chords(song_chords: str):
-    song_chords_in_lines = song_chords.splitlines()
-    clean_chords = []
-    for line in song_chords_in_lines:
-        line = line.strip()
-        bars_in_line = re.findall(r"\|([*.)\|", line)
-        clean_chords.extend(bars_in_line)
-    # NEW system idea:
-    # for every bar in bars
-        # for every key, chord_prog of forms of chord prog
-            # if the first chord_prog in the list is the first 
-        # if(bar has multiple words):
-            # make each word its own separate bar with | | prefix and suffix
-        
-
-    clean_chords_string = "|".join(clean_chords)
-    for char in LIST_REMOVE_OUT_STRING:
-        clean_chords_string = clean_chords_string.replace(char, "")
-
-    return clean_chords_string
+# extracts strings of chord data out of majmin.lab files 
+def parse_lab_chords(song_chords: str):
+    song_chords_per_line = song_chords.splitlines()
+    chord_list = [line.strip().split()[-1] for line in song_chords_per_line if line.strip()]
+    return chord_list
 
 # for every element in dataset (iterate, it's an array)
 for element in READ_DATASET:
     use_of_chord_prog_counter = 0
 
-    #   open the salami_chords.txt file corresponding to the id of this element
+    #   open the majmin.lab file corresponding to the id of this element
     try:
-        with open(rf"C:\Users\pijonka\Documents\PWS\data\q3\dataset-mcgill\McGill-Billboard\{element["id"].zfill(4)}\salami_chords.txt") as f:
+        with open(rf"C:\Users\pijonka\Documents\PWS\data\q3\dataset-mcgill\McGill-Billboard\{element["id"].zfill(4)}\majmin.lab") as f:
             unmod_song_chords = f.read()
     except:
         continue
 
-    # clean chords formatted to work with the finder thing
-    song_chords = parse_salami_chords(unmod_song_chords)
+    # clean chords list
+    song_chords_list = parse_lab_chords(unmod_song_chords)
     
     #   for every key-value pair in FORMS_OF_CHORD_PROG literal:   
-    for key, val in FORMS_OF_CHORD_PROG.items():
-        # if any substring of the song_chords string is equal to the value of this key
-            # add 1 to the use_of_chord_prog_conuter
-            use_of_chord_prog_counter += song_chords.count(val)
-        
+    for key, list_of_chords_in_prog in FORMS_OF_CHORD_PROG.items():
+        # check out each chord in this song
+        for chord in song_chords_list:
+            # if a chord aligns with the first chord of the list
+            if(chord == list_of_chords_in_prog[0]):
+                # check if the next chord is the same chord
+                if(
     #   append to the dataset the use_of_chord_prog_counter
     write_dataset.append({
         "title": element["title"],
@@ -97,7 +82,7 @@ for element in READ_DATASET:
         })
 
 # write to a new dataset file the contents of write_dataset
-with open("pi-mcgill.json", "w") as f:
+with open(fr"C:\Users\pijonka\Documents\PWS\data\q3\pi-mcgill.json", "w") as f:
     json.dump(write_dataset, f, indent=4)
     
 # print(write_dataset)
