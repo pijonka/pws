13c99b817581c6952b0bafc5a5f81f351191f385
pijonka <jon.d.elkouby@gmail.com>
2026-09-20 16:23:09 +0200

    fix(q2): find absolute godsend of an argument to deal with trailing comma issue

--- data/q2/results.py
+++ data/q2/results.py
@@ -1,9 +1,5 @@
 import matplotlib as plt
 import pandas
 
-dataset = pandas.read_csv(R"C:\Users\pijonka\Documents\PWS\data\q2\q2 results exported from google sheets.csv").dropna(how="all", axis=1)
+dataset = pandas.read_csv(R"C:\Users\pijonka\Documents\PWS\data\q2\q2 results exported from google sheets.csv", usecols=range(13))
 
-print(dataset)
-
-print(dataset["frag:A"])
-# dataset_stats = dataset["frag:B"].tolist()
