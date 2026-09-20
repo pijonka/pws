5621a3a4e2f936c1fb22d1d7f3d83f89c9b699e1
pijonka <jon.d.elkouby@gmail.com>
2026-09-20 16:33:46 +0200

    test(q2): retrieve useful test results Output of results.py script: [2, 3, 4, 1, 1, 3, 1, 1, 3, 2, 2, 3, 1, 2, 1, 3, 3, 2, 2, 1, 2, 2, 3, 1, 3, 4, 1, 1, 2, 3, 2, 2, 3, 4, 3, 4] 2.25 0.996422170998389

--- data/q2/results.py
+++ data/q2/results.py
@@ -1,5 +1,17 @@
 import matplotlib as plt
 import pandas
+import math
+import statistics 
 
 dataset = pandas.read_csv(R"C:\Users\pijonka\Documents\PWS\data\q2\q2 results exported from google sheets.csv", usecols=range(13))
 
+# TEST
+frag_results_list = dataset["frag:A"].tolist()
+# calculate mean
+
+# calculate stdev
+
+# output matplotlib trust interval or whatever
+print(frag_results_list)
+print(statistics.mean(frag_results_list))
+print(statistics.stdev(frag_results_list))
